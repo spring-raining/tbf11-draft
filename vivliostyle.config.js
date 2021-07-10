92a7fcaca650b99f1b952800aed8cedfa0eb1424
spring-raining <harusamex.com@gmail.com>
2021-07-11 00:13:51 +0900

    Use screen CSS

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -2,8 +2,8 @@ module.exports = {
   title: 'Vivliostyle で本を作ろう Vol. 5', // populated into 'publication.json', default to 'title' of the first entry or 'name' in 'package.json'.
   author: 'Vivliostyle ユーザー会', // default to 'author' in 'package.json' or undefined
   language: 'ja',
-  size: 'A4',
-  theme: 'theme/theme_print.css', // .css or local dir or npm package. default to undefined
+  // size: 'JIS-B5',
+  theme: 'theme/theme_screen.css', // .css or local dir or npm package. default to undefined
   entry: [
     {
       path: 'content/index.html',
